8daeca57117fbfbcf414cf4ae5e4a238ff314f52
BrandonHaripersad <brandon.haripersad@gmail.com>
2020-02-20 00:07:41 -0500

    Tweets will automatically refresh when they are submitted

--- Client/client.js
+++ Client/client.js
@@ -32,24 +32,29 @@ form.addEventListener('submit', (event) => {
 	  	console.log(createdTweet);
 		form.reset();
 		form.style.display = '';
+		listAllTweets();
 		loadingElement.style.display = 'none';
 	  });
 
 });
 
 function listAllTweets() {
+	tweetEle.innerHTML = '';
 	fetch(API_URL)
 		.then(response => response.json())
 		.then(tweets => {
-			console.log(tweets);
+			tweets.reverse();
 			tweets.forEach(tweet => {
 				const div = document.createElement('div');
 				const header = document.createElement('h3');
 				header.textContent = tweet.name;
 				const contents = document.createElement('p');
 				contents.textContent = tweet.content;
+				const date = document.createElement('small');
+				date.textContent = new Date(tweet.created);
 				div.appendChild(header);
 				div.appendChild(contents);
+				div.appendChild(date);
 				tweetEle.appendChild(div);
 			});
 			loadingElement.style.display = 'none';
